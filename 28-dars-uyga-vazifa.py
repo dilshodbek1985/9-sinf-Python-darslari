e73a8121be7e7f6b97c6781c85bc811125b65424
Dilshodbek Xurboyev <105189019+dilshodbek1985@users.noreply.github.com>
2023-05-20 07:51:08 +0300

    Add files via upload

---
+++ 28-dars-uyga-vazifa.py
@@ -0,0 +1,45 @@
+# Darvoaning eni 4 metr, bo'yi 3 metr"
+# 1) darvoza yuzasi (S) ni hisoblash dasturini tuzing:
+# Masalaningning formulasi S = a * b
+# Javob
+print(4 * 3)
+# 2) Darvoza qirrasi uzunligi (P) ni hisoblash dasturini tuzing
+# Masalaningning formulasi p = (a + b ) * 2
+# Javob
+print((4 + 3) * 2)
+
+# Ikki qayiq turg'un suvda bir - biriga tomon 4 km/soat va 2 km/soat tezlik bilan suzmoqda.
+# Ular orasidagi masofa 24 km bo'lsa:
+# 1) Ular qancha vaqtdan keyin uchrashadi?
+# Masalaningning formulasi t = s / (v1 + v2)
+# Javob
+v1 = 4
+v2 = 2
+s = 24
+t = s / (v1 + v2)
+print(t)
+
+# 2) Qancha vaqtdan keyin ular orasidagi masofa 12 km tashkil etadi?
+# Masalaningning formulasi t = (s - q) / (v1 + v2)
+# Javob
+v1 = 4
+v2 = 2
+s = 24
+q = 12
+t = (s - q) / (v1 + v2)
+print(t)
+
+# Doiraning radiusi 4 metrga teng (PI = 3.14):
+# 1) Doirani yuzini hisoblang:
+# Masalaningning formulasi s = PI*r2
+# Javob
+PI = 3.14
+r = 4
+s = PI * r ** 2
+print(s)
+
+# 2) Aylana uzunligini hisoblang:
+# Masalaningning formulasi L = 2 * PI * r
+# Javob
+L = 2 * PI * r
+print(L)
